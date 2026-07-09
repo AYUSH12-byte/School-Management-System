f924e12062fd9ba30cd9693ed39acf8222c2e633
Ayush <achy93858@gmail.com>
2026-07-09 21:04:47 +0545

    Add Course Controllerl

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use App\Models\Department;
 
+//courseController handles the logic for displaying courses in the school management system.
+
 class CourseController extends Controller
 {
     public function index()
